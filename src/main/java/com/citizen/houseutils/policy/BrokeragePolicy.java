9e158382d4405701477de083c315ce66175296b8
Min Heo <min103ju@gmail.com>
2022-02-12 00:24:08 +0900

    if문을 객체의 관계로 로직을 수정

--- src/main/java/com/citizen/houseutils/policy/BrokeragePolicy.java
+++ src/main/java/com/citizen/houseutils/policy/BrokeragePolicy.java
@@ -1,13 +1,22 @@
 package com.citizen.houseutils.policy;
 
+import com.citizen.houseutils.exception.ErrorCode;
+import com.citizen.houseutils.exception.HouseUtilsException;
+import java.util.List;
+
 /**
  * author : citizen103
  */
 public interface BrokeragePolicy {
 
-    BrokerageRule createBrokerageRule(Long price);
+    List<BrokerageRule> getRules();
 
     default Long calculate(Long price) {
-        return createBrokerageRule(price).calcMaxBrokerage(price);
+        BrokerageRule brokerageRule = getRules().stream()
+            .filter(rule -> price < rule.getLessThan())
+            .findFirst()
+            .orElseThrow(() -> new HouseUtilsException(ErrorCode.INTERNAL_ERROR));
+
+        return brokerageRule.calcMaxBrokerage(price);
     }
 }
--- src/main/java/com/citizen/houseutils/policy/BrokerageRule.java
+++ src/main/java/com/citizen/houseutils/policy/BrokerageRule.java
@@ -1,6 +1,7 @@
 package com.citizen.houseutils.policy;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.lang.Nullable;
 
 /**
@@ -9,7 +10,9 @@ import org.springframework.lang.Nullable;
  * 가격이 특정 범위일 때 상한효율과 상한금액을 가지는 클래스
  */
 @AllArgsConstructor
+@Getter
 public class BrokerageRule {
+    private Long lessThan;
     private Double brokeragePercent;
 
     @Nullable
